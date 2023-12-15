4fd531e508513718c12fb370e92b5a3a7e420291
Pampaj <dawidkawinski@Opoczta.pl>
2023-12-15 23:42:02 +0100

    Fixed svg images

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,15 @@
 import burger from './assets/media/burger.svg';
+import nuggets from './assets/media/nuggets.svg';
+import wrap from './assets/media/wrap.svg';
 
 
 function Home() {
 return (
   <>
-    <img src={burger} alt=""/>
-    <img src="" />
-    <img src="" />
-    <Loading />
+    <img src={burger} alt="burger.svg"/>
+    <img src={nuggets} alt="nuggets.svg"/>
+    <img src={wrap} alt="wrap.svg"/>
+    
   </>
 )
 }
@@ -16,7 +18,7 @@ return (
 function Loading() {
 return (
   <>
-    <p>dsasasdas</p>
+
   </>
 )
 }
