f7a63e85a9e7f0d9671ff73c10eff2d63dc47de0
Pampaj <dawidkawinski@Opoczta.pl>
2023-12-15 22:02:10 +0100

    add something

---
+++ src/Home.jsx
@@ -0,0 +1,25 @@
+import burger from './assets/media/burger.svg';
+
+
+function Home() {
+return (
+  <>
+    <img src={burger} alt=""/>
+    <img src="" />
+    <img src="" />
+    <Loading />
+  </>
+)
+}
+
+
+function Loading() {
+return (
+  <>
+    <p>dsasasdas</p>
+  </>
+)
+}
+
+
+export default Home;
