315819782b7e757dc0c6f9ad8ce6f1ca3b526c74
Pampaj <dawidkawinski@Opoczta.pl>
2024-01-12 00:49:01 +0100

    Added a lot of content in cart template

--- src/templates/Cart.jsx
+++ src/templates/Cart.jsx
@@ -1,19 +1,12 @@
 import Menu from '../components/Menu.jsx'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faXmark } from '@fortawesome/free-solid-svg-icons'
+import items from '../items.js'
+
 
 function Cart() {
 
-  
   const price = 12
-
-  return (
-    <NavBar />
-  )
-}
-
-function NavBar() {
-
   const itemCount = 1;
 
   return (
@@ -23,15 +16,52 @@ function NavBar() {
         <h1 className="text-5xl ml-8 -mt-2">Basket</h1>
         <span className="ml-auto text-xl">Item count: {itemCount}</span>
       </nav>
+      <div className="flex flex-col justify-center items-center">
+        <ItemsList />
+        <OptionMenu />
+      </div>
     </div>
   )
 }
 
 function ItemsList() {
   return (
-    <div>
-      
+    <div className="">
+      <Item />
+      <Item />
     </div>
+  )
+}
+
+function Item() {
+  return (
+    <div className="bg-[#ffff] w-[95vw] h-[120px] rounded-lg shadow-md my-5 p-3">
+      <div className=""></div>
+    </div>
+  )
+}
+
+function OptionMenu() {
+  return (
+    <div className="mt-6 h-full w-[95vw] rounded-md border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
+        <div className="mb-2 flex justify-between">
+          <p className="text-gray-700">Subtotal</p>
+          <p className="text-gray-700">$129.99</p>
+        </div>
+        <div className="flex justify-between">
+          <p className="text-gray-700">Shipping</p>
+          <p className="text-gray-700">$4.99</p>
+        </div>
+        <hr className="my-4" />
+        <div className="flex justify-between">
+          <p className="text-lg font-bold">Total</p>
+          <div className="">
+            <p className="mb-1 text-lg font-bold">$134.98 USD</p>
+            <p className="text-sm text-gray-700">including VAT</p>
+          </div>
+        </div>
+        <button className="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">Check out</button>
+      </div>
   )
 }
 
