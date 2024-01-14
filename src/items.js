8ffe3fd69764fda3d93f09f3d7556d8394498213
McRaZick <mcrazick@mail.com>
2024-01-14 06:03:55 +0000

    Added detailed view of food items upon click, and made it auto navigable

--- src/items.js
+++ src/items.js
@@ -8,8 +8,44 @@ const items = [
   {
     image: require(PATH+'Fresh beef burger isolated -1.png'),
     title: 'beef burger',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    price: 15.23,
+  },
+  {
+    image: require(PATH+'delicious-burguer-isolated-white-background.png'),
+    title: 'burger',
     description: '',
     price: null,
+  },
+  {
+    image: require(PATH+'baguette-sandwich.png'),
+    title: 'baguette',
+    description: '',
+    price: null,
+  },
+  {
+    image: require(PATH+'fresh-tasty-sandwich.png'),
+    title: 'sandwich',
+    description: '',
+    price: null,
+  },
+  {
+    image: require(PATH+'Delicious fried chicken with french fries -9.png'),
+    title: 'chicken n\' fries',
+    description: '',
+    price: null,
+  },
+  {
+    image: require(PATH+'milkshake.png'),
+    title: 'milkshake',
+    description: '',
+    price: null,
+  },
+  {
+    image: require(PATH+'Fresh beef burger isolated -1.png'),
+    title: 'beef burger',
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    price: 15.23,
   },
   {
     image: require(PATH+'delicious-burguer-isolated-white-background.png'),
--- src/templates/Home.jsx
+++ src/templates/Home.jsx
@@ -3,7 +3,7 @@ import '../css/Home.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBasketShopping } from '@fortawesome/free-solid-svg-icons'
 
-import { useEffect, useContext } from 'react'
+import { useEffect, useContext, useState } from 'react'
 import { TemplateContext } from '../App'
 
 import Menu from '../components/Menu.jsx'
@@ -12,8 +12,53 @@ import items from '../items.js'
 
 
 
+
 function Home() {
   const { template, setTemplate } = useContext(TemplateContext);
+  const [ clickedItem, setClickedItem ] = useState(null);
+
+
+  const unselectClickedFoodItem = (event) => {
+    const itemDetails = document.querySelector(".item .details") || null;
+    if (!itemDetails) return;
+
+    const foodItem = itemDetails.parentElement;
+    const isClickInside = foodItem.contains(event.target);
+    if (!isClickInside) setClickedItem(null);
+  }
+
+
+  const scrollToFoodItem = (foodItemID) => {
+    const main = document.querySelector("#Home > main") || null;
+    const foodItem = document.querySelector("#"+foodItemID) || null;
+    if (!foodItem || !main) return;
+
+    // Instantly space-out the (#Home > main) element
+    // to make a smooth center-scroll-animation to the foodItem element 
+    main.style.setProperty("margin-bottom", "100%");
+    main.style.removeProperty("transition");
+ 
+    setTimeout(() => {
+      foodItem.scrollIntoView({ behavior: "smooth", block: "center" });
+    }, 0);
+  }
+
+
+  const onTemplateClick = (event) => {
+    unselectClickedFoodItem(event);
+
+    // IF itemDetails (.details) does not exist, THEN
+    // Smoothly remove the: (#Home > main) element's bottom-margin
+    setTimeout(() => {
+      const main = document.querySelector("#Home > main") || null;
+      const itemDetails = document.querySelector(".item .details") || null;
+      if (!main || itemDetails) return;
+
+      main.style.removeProperty("margin-bottom");
+      main.style.setProperty("transition", "margin 1s linear");
+    }, 100);
+  }
+
 
   useEffect(() => {
     const templateHome = document.querySelector("#Home"); 
@@ -30,10 +75,11 @@ function Home() {
     }, 100);
   })
 
+
   let itemDelay = 0;
 
   return (
-    <div id="Home" className="template-page bg-[#FFF7EB] min-h-[100vh] px-3" display="false">
+    <div id="Home" onClick={(event) => onTemplateClick(event)} className="template-page bg-[#FFF7EB] min-h-[100vh] px-3 pb-5" display="false">
       <header className="banner relative flex justify-center">
         <img className="h-[350px] max-w-[300px]" src={banner} alt="FlavorA banner" />
       </header>
@@ -43,16 +89,42 @@ function Home() {
         </a>
         <Menu />
       </nav>
-      <main className="w-[100%] max-w-[1000px] mx-auto" tabIndex="0">
+      <main className="w-full max-w-[500px] mx-auto" tabIndex="0">
         <button type="button" title="promotion" className="promotion block my-5 mx-auto rounded-md w-full min-h-[150px] bg-[#df9b6a] shadow-xl"></button> 
-        <section className="flex flex-wrap justify-center gap-3">
-          { items.map(item => {
+        <section className="flex flex-wrap justify-center gap-3 relative">
+          { items.map((item, index) => {
             itemDelay += .1; // seconds
             itemDelay = Number(itemDelay.toFixed(2));
+
+            
+            let itemClass = "item";
+            let extendedContainer = null;
+
+            if (clickedItem === index) {
+              itemClass += " flex justify-center w-full";
+              extendedContainer = (
+                <section className="details max-h-[150px] bg-white h-full w-full overflow-hidden p-2 relative shadow-xl rounded-r-md">
+                  <h1 className="item-title text-lg font-medium capitalize truncate">{item.title}</h1>
+                  <hr></hr>
+                  <p className="item-description text-sm break-all line-clamp-3 mb-10">{items[0].description/*item.description*/}</p>
+                  <div className="absolute bottom-0 mb-[5px] w-[calc(100%-10px)]">
+                    <span className="float-left item-title text-lg font-bold capitalize truncate">${(Math.random() * 100).toFixed(2)/*item.price*/}</span>
+                    <button className="float-right mr-1 bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-4 rounded-md">ADD</button>
+                  </div>
+                </section>
+              );
+            }
+
+            // ID is required for scrollToFoodItem()
+            const id = "foodItem" + index;
+
             return (
-              <button key={itemDelay} type="button" title={item.title} className="item rounded-md w-[150px] h-[150px] text-center shadow-xl translate-y-[500px]" style={{ animationDelay: `${itemDelay}s` }}>
-                <img src={item.image} className="max-h-[100%] mx-auto" alt={item.title} /> 
-              </button>
+              <div id={id} key={index} className={itemClass}>
+                <button type="button" onClick={() => setTimeout(() => {setClickedItem(index); scrollToFoodItem(id)}, 0)} className="img rounded-md w-[150px] h-[150px] text-center shadow-xl translate-y-[500px]" style={{ animationDelay: `${itemDelay}s` }} title={item.title}>
+                  <img src={item.image} className="max-h-[100%] mx-auto" alt={item.title} />
+                </button>
+                {extendedContainer}
+              </div>
             );
           })}
         </section>
@@ -62,44 +134,5 @@ function Home() {
 }
 
 
-
-function FoodItemContainer() {
-  const items = [{
-    "image": "../assets/images/items/Fresh beef burger isolated -1.png",
-    "title": "burger",
-    "description": "",
-    "price": ""
-  },
-  {
-    "image": "#",
-    "title": "wrap",
-    "description": "",
-    "price": ""
-  },
-  {
-    "image": "#",
-    "title": "3",
-    "description": "",
-    "price": ""
-  },
-  {
-    "image": "#",
-    "title": "4",
-    "description": "",
-    "price": ""
-  }];
-
-  return (
-    <div className="flex flex-wrap justify-center gap-3">
-      { items.map(item => {
-        return (
-          <div className="item rounded-md w-[48%] min-h-[150px] bg-[#df9b6a] text-center shadow-xl">
-            <img src="" /> 
-          </div>
-        );
-      })}
-    </div>
-  );
-}
 
 export default Home;
